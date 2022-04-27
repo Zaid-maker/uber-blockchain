2f372580d7f139a8896c59a88f59ef3cf2b10dd8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-04-27 19:20:00 +0500

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Image from 'next/image'
+import { BsPerson } from 'react-icons/bs'
 
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
@@ -12,8 +14,34 @@ const style = {
   loginText: `ml-2`,
 }
 
+const currentAccount = '0x0'
+
 const Navbar = () => {
-  return <div className={style.wrapper}>Navbar</div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.leftMenu}>
+        <div className={style.logo}>Uber</div>
+        <div className={style.menuItem}>Home</div>
+        <div className={style.menuItem}>Drive</div>
+        <div className={style.menuItem}>More</div>
+      </div>
+      <div className={style.rightMenu}>
+        <div className={style.menuItem}>Help</div>
+        <div className={style.menuItem}>DEVMIRZA</div>
+        <div className={style.userImageContainer}></div>
+        {currentAccount ? (
+          <div>
+            {currentAccount.slice(0, 5)}...{currentAccount.slice(39)}
+          </div>
+        ) : (
+          <div className={style.loginButton}>
+            <BsPerson />
+            <span className={style.loginText}>Login</span>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
